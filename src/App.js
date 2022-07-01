3124a08bc7a7acabd7dc78c8b61b62456c1e5ad9
mattyl006 <mattyl@st.amu.edu.pl>
2022-07-01 15:38:05 +0200

    styled-components and react-media

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import styled from 'styled-components';
+import Media from 'react-media';
+
+const H1 = styled.h1`
+  color: green;
+`
+
 function App() {
-  return (
-      <h1>
-        Hello Gonito!
-      </h1>
-  );
+    return (
+        <>
+            <Media query="(max-width: 768px)">
+                <H1>
+                    Hello Gonito mobile!
+                </H1>
+            </Media>
+            <Media query="(min-width: 768px)">
+                <H1>
+                    Hello Gonito desktop!
+                </H1>
+            </Media>
+        </>
+    );
 }
 
 export default App;
