d3f1bc81f97996a4ea449d037ce050f2507481b3
Mateusz Tylka <mattyl@st.amu.edu.pl>
2023-11-17 12:58:31 +0100

    hard code old api path in Readme request

--- src/api/getChallengeFullDescription.js
+++ src/api/getChallengeFullDescription.js
@@ -1,13 +1,17 @@
-import {API} from '../utils/globals';
+// import { API } from '../utils/globals';
 
-const getChallengeFullDescription = (setDataState, setLoading, challengeName) => {
-    fetch(`${API}/challenge-readme/${challengeName}/markdown`)
-        .then(response => response.text())
-        .then(data => {
-            setDataState(data);
-            if (setLoading)
-                setLoading(false);
-        });
+const getChallengeFullDescription = (
+  setDataState,
+  setLoading,
+  challengeName
+) => {
+  // TODO: insert challenge repos on our server to use API variable
+  fetch(`https://gonito.net/api/challenge-readme/${challengeName}/markdown`)
+    .then((response) => response.text())
+    .then((data) => {
+      setDataState(data);
+      if (setLoading) setLoading(false);
+    });
 };
 
 export default getChallengeFullDescription;
