663d3af6b51b1371c9d19a73b8d2de2f9f1fe089
Szymon Bartanowicz <szymon@bartanowicz.pl>
2024-04-10 19:40:16 +0200

    cr fix

--- src/components/navigation/NavBar/styles/NavBarStyle.js
+++ src/components/navigation/NavBar/styles/NavBarStyle.js
@@ -3,39 +3,6 @@ import { Container } from '../../../../utils/containers';
 import colors from "../../../../utils/colors";
 
 const NavBarStyle = styled(Container)`
-  //   position: fixed;
-  //   top: 0;
-  //   left: 0;
-  //   width: 100%;
-  //   height: 64px;
-  //   z-index: 999;
-  //
-  //   nav {
-  //     margin: 0 auto;
-  //     max-width: 1440px;
-  //     padding: 0 16px;
-  //     background: ${colors.white};
-  //   }
-  //
-  // .border {
-  //   border-bottom: 1px solid ${colors.black700};
-  //   width: 70%;
-  //   margin: 0 auto;
-  // }
-  //
-  // .NavbarStyle__sign_in_btn {
-  //   color: ${colors.black700};
-  //   border: 2px solid ${colors.green500};
-  //   border-radius: 40px;
-  //   background: ${colors.white};
-  //   padding: 8px 24px;
-  // }
-  //
-  // .NavbarStyle__links li{
-  //   color: ${colors.black900};
-  //   font-size: 19px;
-  // }
-  
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: ${({ theme }) => theme.navShadow};
   padding: 0 16px;
--- src/pages/LandingPage/LandingPageStyle.js
+++ src/pages/LandingPage/LandingPageStyle.js
@@ -5,24 +5,6 @@ const LandingPageStyle = styled(FlexColumn)`
   max-width: 1440px;
   margin: 0 auto;
   padding: 0 64px;
-  //justify-content: flex-start;
-  //width: 100%;
-  //min-height: 100vh;
-  //padding: 0 32px;
-  
-  //.LandingPageStyle__main-container {
-   //max-width: 452px;
-   //gap: 48px;
-   //width: 80%;
-  //}
-
-  // @media (min-width: ${({ theme }) => theme.overMobile}) {
-  //   padding: 0 124px 124px;
-  //   .LandingPageStyle__main-container {
-  //     max-width: none;
-  //     gap: 124px;
-  //   }
-  // }
 `;
 
 export default LandingPageStyle;
